242b587c7ae50f3a9e1127a8821e7ab3f8fc5e1a
igornfaustino <igor@nfaustino.com>
2021-12-07 22:53:09 -0300

    feat: add screen size hook

--- example/index.tsx
+++ example/index.tsx
@@ -1,12 +1,53 @@
 import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Thing } from '../.';
+import { useScreenSize } from '../.';
 
 const App = () => {
+  const {
+    isDesktop,
+    isTablet,
+    isMobile,
+    isLargeMobile,
+    isLargeDesktop,
+    screenSize,
+  } = useScreenSize();
+
   return (
     <div>
-      <Thing />
+      <h1>Resize me!</h1>
+
+      <h2>Screen size</h2>
+      <p>
+        Width: <b>{screenSize.width}</b>
+      </p>
+      <p>
+        height: <b>{screenSize.height}</b>
+      </p>
+
+      <h2>Devices helper</h2>
+
+      <p>
+        isLargeDesktop (screen over 1200px):{' '}
+        <b>{isLargeDesktop ? 'True' : 'False'}</b>
+      </p>
+
+      <p>
+        isDesktop (screen over 992px): <b>{isDesktop ? 'True' : 'False'}</b>
+      </p>
+
+      <p>
+        isTablet (screen over 768px): <b>{isTablet ? 'True' : 'False'}</b>
+      </p>
+
+      <p>
+        isLargeMobile (screen over 576px):{' '}
+        <b>{isLargeMobile ? 'True' : 'False'}</b>
+      </p>
+
+      <p>
+        isMobile (screen bellow 576px): <b>{isMobile ? 'True' : 'False'}</b>
+      </p>
     </div>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,35 @@
-import * as React from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
-// Delete me
-export const Thing = () => {
-  return <div>the snozzberries taste like snozzberries</div>;
+export const useScreenSize = () => {
+  const isClient = typeof window === 'object';
+
+  const getScreenSize = useCallback(
+    () => ({
+      width: isClient ? window.innerWidth : 0,
+      height: isClient ? window.innerHeight : 0,
+    }),
+    [isClient]
+  );
+
+  const [screenSize, setScreenSize] = useState(getScreenSize);
+
+  useEffect(() => {
+    if (!isClient) return;
+
+    function handleResize() {
+      setScreenSize(getScreenSize());
+    }
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, [getScreenSize, isClient]);
+
+  return {
+    screenSize,
+    isMobile: screenSize.width < 576,
+    isLargeMobile: screenSize.width >= 576,
+    isTablet: screenSize.width >= 768,
+    isDesktop: screenSize.width >= 992,
+    isLargeDesktop: screenSize.width >= 1200,
+  };
 };
